fe3df1e28cd76cc53ab4cf476d3134a0d5d8cc64
thevictorwhyte <victordavidwhyte@gmail.com>
2022-06-30 23:29:52 +0100

    test: user can't enter lottery unless it has started

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -1,8 +1,12 @@
-from brownie import Lottery, accounts, config, network
+from brownie import Lottery, accounts, config, network, exceptions
 from scripts.deploy_lottery import deploy_lottery
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
 from web3 import Web3
+import pytest
 
 def test_get_entrance_fee():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
     # Arrange
     lottery = deploy_lottery()
     # Act
@@ -13,4 +17,13 @@ def test_get_entrance_fee():
     entrance_fee = lottery.getEntranceFee()
     # Assert
     assert expected_entrace_fee == entrance_fee
+
+def test_cant_enter_unless_started():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    # Act / Assert
+    with pytest.raises(exceptions.VirtualMachineError):
+        lottery.enter({"from": get_account(), "value": lottery.getEntranceFee() + 10000000})
 
